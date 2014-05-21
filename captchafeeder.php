224449e961de7406b57f858c0ea3ec9851a67bed
Marina Brown <catskillmarina@gmail.com>
2014-05-21 09:34:38 -0400

    starting to make this output pipe delimited

--- captchafeeder.php
+++ captchafeeder.php
@@ -1,6 +1,4 @@
 <?php
-    /* require_once(captchafeeder-mysql.php); */
- 
     include_once('captchafeeder-conf.php');
 
     // Connecting, selecting database //
@@ -17,22 +15,18 @@
 
     // Performing SQL query //
 
-    $query = 'SELECT * FROM my_table';
+    $query = 'SELECT serial,question,answer FROM captchafeeder ORDER by random LIMIT 1';
     $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
     // Printing results in HTML //
 
-    echo "<table>\n";
     while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
     {
-        echo "\t<tr>\n";
         foreach ($line as $col_value)
         {
-            echo "\t\t<td>$col_value</td>\n";
+            echo "$col_value|\n";
         }
-        echo "\t</tr>\n";
     }
-    echo "</table>\n";
 
     // Free resultset
     mysql_free_result($result);
--- captchafeeder.sql
+++ captchafeeder.sql
@@ -1,5 +1,5 @@
 CREATE TABLE captcafeeder IF NOT EXISTS `$name_escaped` (
-    `serialnum`   INT NOT NULL AUTO_INCREMENT,
+    `serial`   INT NOT NULL AUTO_INCREMENT,
     `question`    VARCHAR(240) NOT NULL,
     `answer`      VARCHAR(80) NOT NULL,
     `createdate`  DATETIME NOT NULL default '0000-00-00 00:00:00,
