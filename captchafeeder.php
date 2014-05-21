5e57a0f42355b0e9dddcbb078be9dff79ee3b162
Marina Brown <catskillmarina@gmail.com>
2014-05-21 22:35:26 +0200

    basic app is working

--- captchafeeder.php
+++ captchafeeder.php
@@ -1,52 +1,76 @@
 <?php
     include_once('captchafeeder-conf.php');
 
-    // Connecting, selecting database //
+
+    $ipaddress = $_SERVER["REMOTE_ADDR"];
+    
+    $ok = 0;
+    foreach ($captchafeeder_allowed_ip as $allowed)
+    {
+        if (preg_match("/$allowed/", $ipaddress))
+        {
+            $ok = 1;
+        }
+    }
+    if(! $ok)
+    {
+        echo "GO AWAY $ipaddress !!! YOU ARE NOT ALLOWED HERE !!\n";
+        die ();
+    }
 
     $link = mysql_connect($captchafeeder_db_host,
                           $captchafeeder_db_user,
                           $captchafeeder_db_mysql_password)
-        or die('Could not connect: ' . mysql_error());
+        or die('Could not connect: ' . mysql_error() . "\n");
 
-    echo 'Connected successfully';
 
     mysql_select_db($captchafeeder_db) 
-        or die('Could not select database');
+        or die("Could not select database\n");
 
-    // Performing SQL query //
+    $query = 'SELECT serial,question,answer FROM captchafeeder LIMIT 1;';
 
-    $query = 'SELECT serial,question,answer 
-                  FROM captchafeeder 
-                  ORDER by random 
-                  LIMIT 1';
+    $result = mysql_query($query) 
+        or die('Query1 failed: ' . mysql_error() . "\n");
 
-    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-    $serial = $line[0];
-
-    $query = "DELETE from captchafeeder WHERE serial = $serial";
-
-    $result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-    // Printing results in HTML //
+    $i = 0;
+    $serial = "";
 
     while ($line = mysql_fetch_array($result, MYSQL_ASSOC))
     {
-        $i = 0;
+        $j = 0;
         foreach ($line as $col_value)
         {
-            echo "$col_value|\n";
+            if($j == 0)
+            {
+                $serial = $col_value;
+            }
+            if($j == 1)
+            {
+                echo "$col_value|";
+            }
+            if($j == 2)
+            {
+                echo "$col_value";
+            }
+            $j++;
             $i = 7;
         }
+        echo "\n";
     }
-    if($i == 0)
-    {
-        echo "What meat do most americans eat at thanksgiving ?|turkey\n";
-    }
-
-    // Free resultset
     mysql_free_result($result);
 
-    // Closing connection
+
+    if($i == 0)
+    {
+        echo "$captchafeeder_noammo_question";
+    }
+    else
+    {
+        $query = "DELETE from captchafeeder WHERE serial = $serial;";
+        $result = mysql_query($query) or die('Query2 failed: ' . mysql_error());
+        echo "i think i deleted\n";
+        mysql_free_result($result);
+    }
+
     mysql_close($link);
 ?>
--- captchafeeder.sql
+++ captchafeeder.sql
@@ -1,11 +1,9 @@
-CREATE TABLE captcafeeder IF NOT EXISTS `$name_escaped` (
+CREATE TABLE captchafeeder (
     `serial`   INT NOT NULL AUTO_INCREMENT,
     `question`    VARCHAR(240) NOT NULL,
     `answer`      VARCHAR(80) NOT NULL,
-    `createdate`  DATETIME NOT NULL default '0000-00-00 00:00:00,
     `lastaccess`  TIMESTAMP,
     `accesses`    INT,
-    `random`      VARCHAR(133),
-    PIMARY KEY (`random`)
+    PRIMARY KEY (serial)
 );
-CREATE INDEX on captchafeeder (`random`);
+CREATE INDEX on captchafeeder (`serial`);
